c057b38d90d03a3be87a4b75fdf9dde5a5b8fa65
Kariny Leandro <karinyleandro345@gmail.com>
2025-06-19 17:11:11 -0300

    EU NAO AGUENTO MAIS, SOCORROOOOOOOOOOOO

--- Lumyk-copy/manage.py
+++ Lumyk-copy/manage.py
@@ -7,5 +7,10 @@ app = create_app()
 cli = FlaskGroup(app)
 migrate = Migrate(app, db)
 
+
+@app.route("/")
+def home():
+    return "Flask está funcionando! 🚀"
+
 if __name__ == "__main__":
     cli()
--- Lumyk-copy/run.py
+++ Lumyk-copy/run.py
@@ -2,6 +2,10 @@ from backend.app import create_app
 
 app = create_app()
 
+@app.route("/")
+def home():
+    return "Flask está funcionando! 🚀"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
 
