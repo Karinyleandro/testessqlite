9e45e918657133281194717159672c7426e20d0a
Kariny Leandro <karinyleandro345@gmail.com>
2025-06-19 21:41:05 -0300

    I BELIEVE IN THE POWER OF LOVER I THINK EU TO COM SONO

--- Lumyk-copy/backend/app/routes/health_routes.py
+++ Lumyk-copy/backend/app/routes/health_routes.py
@@ -3,6 +3,11 @@ from flask_restx import Namespace, Resource
 api = Namespace('health', description='Health check')
 
 @api.route('/')
+class RootHealthCheck(Resource):
+    def get(self):
+        return {"mensagem": "Backend online! [root]"}, 200
+
+@api.route('/health')
 class HealthCheck(Resource):
     def get(self):
-        return {"mensagem": "Backend online!"}, 200
+        return {"mensagem": "Backend online! [health]"}, 200
